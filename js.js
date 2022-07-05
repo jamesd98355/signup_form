fe72b548c14f2d8fe396a4344adaac7c9c5c62b6
jamesd98355 <james.donnelly246@hotmail.com>
2022-07-05 13:26:40 +0100

    finish validation

---
+++ js.js
@@ -0,0 +1,14 @@
+//confirm passwords are the same
+var password = document.getElementById("password")
+var confirm_password = document.getElementById("confirmpassword");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
